dcaf2a6e366fa839a8b7a8b79dcb8e01c1d726d9
odiunus <falcaofilho20@gmail.com>
2023-10-01 19:44:52 -0300

    refinando exceptions

--- domain/src/main/java/com/fullcycle/admin/catalogo/domain/exceptions/DomainException.java
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/exceptions/DomainException.java
@@ -4,17 +4,24 @@ import com.fullcycle.admin.catalogo.domain.validation.Error;
 
 import java.util.List;
 
-public class DomainException extends RuntimeException{
+public class DomainException extends NoStackTraceException{
 
     private final List<Error> errors;
 
-    protected DomainException(final List<Error> aErros) {
-        super("",null,true,false);
+
+
+    protected DomainException(final String aMessage,final List<Error> aErros) {
+        super(aMessage);
         this.errors = aErros;
     }
 
+    public static DomainException with(final Error aErrors)
+    {
+        return new DomainException(aErrors.message(), List.of(aErrors));
+    }
+
     public static DomainException with(final List<Error> aErrors){
-        return new DomainException(aErrors);
+        return new DomainException("",aErrors);
     }
 
     public List<Error> getErrors() {
